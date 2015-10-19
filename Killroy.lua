b35e8f6f93b56c445241428f4e5d346b00f76553
Benjamin Slack <iam@nimajneb.com>
2015-10-18 21:37:59 -0400

    Turn off Debugging
    Forgot to disabled the Debugger

--- Killroy.lua
+++ Killroy.lua
@@ -248,7 +248,7 @@ function Killroy:OnDocumentLoaded()
 	GeminiColor = Apollo.GetPackage("GeminiColor").tPackage
 	GeminiLogging = Apollo.GetPackage("Gemini:Logging-1.2").tPackage
 	self.glog = GeminiLogging:GetLogger({
-		level = GeminiLogging.DEBUG,
+		level = GeminiLogging.FATAL,
         pattern = "%d %n %c %l - %m",
         appender = "GeminiConsole"
     })
